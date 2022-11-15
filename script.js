a285dc6cd849299302b27a6993b9675a45cba571
MarcosGTS <marcosgtsodre@gmail.com>
2022-11-15 20:06:41 -0300

    add piece placement

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 
-let field = [
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","red","red","",""],
-    ["","","","red","red",""],
-    ["","","","","",""],
-    ["","","","","",""],
-    ["","","","","",""],
-]
+
+
 
 const CELL_SIZE = 50
 
@@ -24,10 +14,85 @@ function render(context, field) {
     }
 }
 
+function createGame() {
+    let state = {
+        field :[
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],],
+
+    }
+
+    let pieces = {
+        "s":[["red","red", ""],
+             ["", "red","red"],
+             ["", "", ""]],
+        "l":[["blue","", ""],
+             ["blue", "",""],
+             ["blue", "blue", ""]],
+    }
+
+    let crrPiece = {
+        "piece": null,
+        "x": 0,
+        "y": 2,
+    }
+
+    crrPiece.piece = pieces.s;
+
+    function placePieceOnField() {
+        clearField()
+        
+        for (let row in crrPiece.piece) {
+            for (let col in crrPiece.piece[0]) {
+                let fieldX = crrPiece.x + parseInt(col)
+                let fieldY = crrPiece.y + parseInt(row)
+                
+                state.field[fieldY][fieldX] = crrPiece.piece[row][col]
+                
+            }
+        }
+    }
+
+    function clearField() {
+        state.field = [
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+            ["","","","","",""],
+        ]
+    }
+
+    function update() {
+        placePieceOnField()
+    }
+
+    return {
+        state,
+        update
+    }
+}
+
 let canvas = document.getElementById("myCanvas")
 let context = canvas.getContext('2d')
+let game = createGame()
 
-canvas.height = field.length * CELL_SIZE
-canvas.width = field[0].length * CELL_SIZE
+canvas.height = game.state.field.length * CELL_SIZE
+canvas.width = game.state.field[0].length * CELL_SIZE
 
-render(context, field)
+game.update()
+console.log(game.state.field)
+render(context, game.state.field)
